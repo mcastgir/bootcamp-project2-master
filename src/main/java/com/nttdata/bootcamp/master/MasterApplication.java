dbdd26d4604ef615209d08d914fb716481ed0641
mcastgir <mcastgir@emeal.nttdata.com>
2022-08-19 13:28:19 -0500

    Add Dockerfile

--- src/main/java/com/nttdata/bootcamp/master/MasterApplication.java
+++ src/main/java/com/nttdata/bootcamp/master/MasterApplication.java
@@ -19,12 +19,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Clase para iniciar el microservicio.
  */
 @EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MasterApplication implements CommandLineRunner {
 
